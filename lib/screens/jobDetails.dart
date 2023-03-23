2fb596efc1901b6df5c785636569068d998d70a6
dinaehab-119 <dinaehab673@gmail.com>
2023-03-24 00:47:07 +0200

    13

---
+++ lib/screens/jobDetails.dart
@@ -0,0 +1,79 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../functions/uoloadAndPridict.dart';
+
+class JobDetails extends StatefulWidget {
+  final String jobTitle;
+  final String jobDescription;
+  final String posterName;
+  final String date;
+  final String jobId;
+
+  const JobDetails(
+      {required this.jobTitle,
+      required this.jobDescription,
+      required this.posterName,
+      required this.date,
+      required this.jobId});
+
+  @override
+  State<JobDetails> createState() => _JobDetailsState();
+}
+
+class _JobDetailsState extends State<JobDetails> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Job Details'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              widget.jobTitle,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              widget.jobDescription,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              widget.date,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              widget.posterName,
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+          ),
+          UploadAndPredict()
+        ],
+      ),
+    );
+  }
+}
